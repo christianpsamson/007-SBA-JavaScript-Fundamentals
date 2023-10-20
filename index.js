ceca46438c15711e769c5a01772d727045755b56
Christian Samson <85580709+christianpsamson@users.noreply.github.com>
2023-10-20 01:03:50 -0400

    Updated: Added new function for final result

--- index.js
+++ index.js
@@ -75,52 +75,107 @@ const LearnerSubmissions = [
     },
   },
 ];
-// ========================================================//
-// Note: Headers were included for SBA purposes only       //
-// ========================================================//
-// ========================================================//
-// Function: Validate input data                           //
-// ========================================================//
+// ====================================================================//
+// Define global variable                                              //
+// ====================================================================//
+let newArray = [];
+let resultArray = [];
+// ====================================================================//
+// Desc: Validate input Course ID & Assignment Group ID                //
+// ====================================================================//
 const validateInputData = function (objName, objID, errMsg) {
   if (!Object.values(objName).includes(objID)) {
     throw errMsg;
   }
 };
-// ========================================================//
-// Function: Create an array                               //
-// ========================================================//
-//update this function to include data from AG
-// to get the following from Assignment Grp - ag.name, ag.due date, and ag.possible points
-// also - l.submission date and l.score
-const createArray = function (learnerID) {
-  let newArray = LearnerSubmissions.filter(
-    (learner) => learner.learner_id == learnerID
-  );
-  console.log(newArray);
+// ====================================================================//
+// Desc: Create an array from Learner Object & Assignment Grp          //
+// ====================================================================//
+
+const createArray = function () {
+  // A. Create new array from Learner:
+  newArray = LearnerSubmissions;
+  // B. Filter Assignment Group according to Assignment ID:
+  for (i = 0; i < LearnerSubmissions.length; i++) {
+    const ag_ID = LearnerSubmissions[i].assignment_id;
+    const addAgData = AssignmentGroup.assignments.filter(
+      (assignmentData) => assignmentData.id === ag_ID
+    );
+    // C. Update the newArray; Note: addAgData length is always 1
+    // hence addAgData[0]
+    newArray[i]["due_date"] = addAgData[0].due_at;
+    newArray[i]["max_points"] = addAgData[0].points_possible;
+  }
 };
-// ========================================================//
-// Main function                                           //
-// ========================================================//
+// ====================================================================//
+// Desc: Generate the final result                                     //
+// ====================================================================//
+// A. Loop through newArray
+const generateResult = function () {
+  let rptId = 0;
+  let tempRptId = 0;
+  let rptAssignId = 0;
+  let rptScore = 0;
+  let rptMaxScore = 0;
+  let rptAccScore = 0;
+  let rptAccMaxScore = 0;
+  let rptAve = 0;
+  let rptAccAve = 0;
+  let rptDueDate = "";
+  let rptSubDate = "";
+  for (i = 0; i < newArray.length; i++) {
+    rptId = newArray[i].learner_id;
+    if (tempRptId !== rptId && i != 0) {
+      rptAccAve = rptAccScore / rptAccMaxScore;
+      
+    } else {
+      tempRptId = rptId;
+      rptAssignId = newArray[i].assignment_id;
+      rptDueDate = new Date(newArray[i].due_date);
+      rptSubDate = new Date(newArray[i].submission.submitted_at);
+      // Validate if late
+      if (rptSubDate > rptDueDate) {
+        rptScore = newArray[i].submission.score - 0.1 * newArray[i].max_points;
+      } else {
+        rptScore = newArray[i].submission.score;
+      }
+      // Validate if due in the future
+      if (rptDueDate > new Date()) {
+        rptScore = 0;
+        rptMaxScore = 0;
+      } else {
+        rptMaxScore = newArray[i].max_points;
+        rptAve = (rptScore / rptMaxScore).toFixed(3);
+        console.log(rptAve);
+      }
+      rptAccMaxScore += rptMaxScore;
+      rptAccScore += rptScore;
+    }
+  }
+};
+// ====================================================================//
+// MAIN FUNCTION                                                       //
+// Note: Headers were included for SBA purposes only                   //
+// ====================================================================//
 function getLearnerData(course, ag, submissions) {
-  //Validate the input data:
+  //Validate the (1)Course ID and (2) Assignment Group ID:
   validateInputData(CourseInfo, course, "Invalid Course ID");
   validateInputData(AssignmentGroup, ag, "Invalid Assignment Group ID");
 
-  // Validate the input learner's ID
-  if (
-    LearnerSubmissions.some(
-      (learnerValid) => learnerValid.learner_id === submissions
-    )
-  ) {
-    createArray(submissions);
-  } else {
-    throw "Invalid Learner ID";
-  }
-
-  // ========================END=============================//
+  // Validate the Learner ID.
+  LearnerSubmissions.forEach((learnerData) => {
+    if (!submissions.includes(learnerData.learner_id))
+      throw "Invalid Learner ID";
+  });
+  createArray();
+  generateResult();
+  // calculating the weighted average
+  // validate the date
+  //
+  // ================================END=================================//
 }
 
-// Code console to get the input data
-const result = getLearnerData(451, 12345, 125);
+// Input data; Note: Learner Submission is an array
+const result = getLearnerData(451, 12345, [125, 132]);
 
 // console.log(result);
